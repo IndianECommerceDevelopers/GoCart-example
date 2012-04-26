e12701a91189526173d9e7f69993bb981eb45016
P R Dinesh <pr.dinesh@live.in>
2012-04-26 13:22:45 +0530

    sdf

--- php/gocart/helpers/MY_url_helper.php
+++ php/gocart/helpers/MY_url_helper.php
@@ -10,28 +10,13 @@ if ( ! function_exists('force_ssl'))
 {
 	function force_ssl()
 	{
-		print_r($_SERVER);
 		
 		if (ssl_support() &&  (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == 'off'))
 		{
 		
-		
 			$CI =& get_instance();
-			$checkreplace =  $CI->config->config['base_url'];
 			$CI->config->config['base_url'] = str_replace('http://', 'https://', $CI->config->config['base_url']);
-			// If we are already on HTTPS and if the Server Variable is not set properly , then avoid endless redirection by checking if there is any change in the url.
-			if(strcmp($checkreplace, $CI->config->config['base_url']))
-			{
-					echo "Before change : ";print_r($checkreplace);
-					echo "After change : ";print_r($CI->config->config['base_url']);
-					die;
-
-				redirect($CI->uri->uri_string());
-			}
-					echo "Before change : ";print_r($checkreplace);
-					echo "After change : ";print_r($CI->config->config['base_url']);
-			die;
-
+			redirect($CI->uri->uri_string());
 		}
 
 	}
